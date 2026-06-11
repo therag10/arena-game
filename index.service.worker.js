5a3b8adde9faa991fc7795f771cee0392830a325
therag10 <theragon10@gmail.com>
2026-06-11 04:50:38 +0300

    update 1.3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '78241D1F4B5A0B23';
+const CACHE_VERSION = '8DE2A1130B214370';
 /** @type {string} */
 const CACHE_PREFIX = 'ARENA-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
